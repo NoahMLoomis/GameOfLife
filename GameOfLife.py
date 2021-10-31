bd1ad736ead185d0cd8fd8e02f84a1757273031b
nloomis <noahmmLoomis@gmail.com>
2021-10-31 19:23:31 -0400

    Stashing

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,8 +1,56 @@
 from Grid import GridClass
+import random, copy
+
 
 class GameOfLife:
-    def __init__(self, game_grid = None):
+    def __init__(self, game_grid=[], universe_size=4):
         self.game_grid = game_grid
+        self.universe_size = universe_size
+
+    def create_grid(self):
+        self.game_grid = GridClass(square_size=self.universe_size)
+
+    def populate_randomized(self):
+        for cell in self.game_grid.grid:
+            if random.random() < 0.4:
+                self.game_grid.change_alive_status(cell)
+
+    def play_sequence(self):
+        grid_copy = copy.deepcopy(self.game_grid)
+        for index in range(len(grid_copy.grid)):
+            if grid_copy.get_cell_alive_status(grid_copy.grid[index]) == 1:
+                self.check_overpopulation(index, grid_copy)
+
+    def check_overpopulation(self, index, grid_copy):
+        alive_count = 0
+        left = grid_copy.grid[index - 1]
+        right = grid_copy.grid[index + 1]
+        top = grid_copy.get_cell(grid_copy.grid[0], grid_copy.grid[1] + 1)
+        # bottom = grid_copy.grid[]
+        # left_bot_diag = grid_copy.grid[]
+        # right_bot_diag = grid_copy.grid[]
+        # left_top_diag = grid_copy.grid[]
+        # right_top_diag = grid_copy.grid[]
+        
+        while not IndexError:
+            if left[2] == 1:
+                alive_count += 1
+            if right[2] == 1:
+                alive_count += 1
+            if alive_count > 3:
+                grid_copy.change_alive_status(grid_copy.grid[index])
+            
         
         
-gof = GameOfLife()
+    # def check_statis(self):
+    
+    # def check_underpopulation(self):
+        
+    
+    # def check_reproduction(self):
+    
+    
+g = GameOfLife()
+g.create_grid()
+g.populate_randomized()
+g.play_sequence()
--- Grid.py
+++ Grid.py
@@ -1,48 +1,64 @@
-import turtle
+# import turtle
 
-SCREEN_SIZE = 600
+# SCREEN_SIZE = 600
 
-t = turtle.Turtle(visible=False)
-wn = turtle.Screen()
-wn.setup(SCREEN_SIZE, SCREEN_SIZE)
-wn.tracer(0)
+# t = turtle.Turtle(visible=False)
+# wn = turtle.Screen()
+# wn.setup(SCREEN_SIZE, SCREEN_SIZE)
+# wn.tracer(0)
 
 class GridClass:
-    def __init__(self, num_rows, grid=[]):
-        self.num_rows = num_rows
-        self.num_cols = num_rows
+    def __init__(self, square_size, grid=[]):
+        self.square_size = square_size
         self.grid = grid
         
         if self.grid == []:
-            for row in range(num_rows):
-                for col in range(num_rows):
+            for row in range(square_size):
+                for col in range(square_size):
                     self.grid.append([row, col, 0])
-
-
-    def draw_square(self, start_x, start_y, size_of_square, is_square_alive):
-        t.goto(start_x, start_y)
-        if is_square_alive == 1:
-                t.fillcolor("green")
-                t.begin_fill()
-        t.pendown()
-        for i in range(4):
-            t.forward(size_of_square)
-            t.right(90)
-        t.end_fill()
-        t.penup()
+                    
+    def change_alive_status(self, cell):
+        # cell = self.get_cell(x_pos, y_pos)
+        if  self.get_cell_alive_status(cell) == 0:
+            cell[2] = 1
+        elif self.get_cell_alive_status(cell) == 1:
+            cell[2] = 0
+            
+    def get_cell(self, x_pos, y_pos):
+        for cell in self.grid:
+            if cell[0] == x_pos and cell[1] == y_pos:
+                return cell
+        return None
     
-    
-    def draw_grid(self):
-        size_of_square = SCREEN_SIZE /self.num_rows
-        t.penup()
-        start_x, start_y = -300, -300
-                
-        for row in range(self.num_rows):
-            start_y += size_of_square
-            start_x = -300
-            for col in range(self.num_rows):
-                self.draw_square(start_x, start_y, size_of_square, self.grid[int(f'{row}{col}')][2])
-                start_x += size_of_square
         
 
-wn.exitonclick()
+    def get_cell_alive_status(self, cell):
+        return cell[2]
+    
+    # def draw_square(self, start_x, start_y, size_of_square, is_square_alive):
+    #     t.goto(start_x, start_y)
+    #     if is_square_alive == 1:
+    #             t.fillcolor("green")
+    #             t.begin_fill()
+    #     t.pendown()
+    #     for i in range(4):
+    #         t.forward(size_of_square)
+    #         t.right(90)
+    #     t.end_fill()
+    #     t.penup()
+    
+    
+    # def draw_grid(self):
+    #     size_of_square = SCREEN_SIZE /self.num_rows
+    #     t.penup()
+    #     start_x, start_y = -300, -300
+                
+    #     for row in range(self.num_rows):
+    #         start_y += size_of_square
+    #         start_x = -300
+    #         for col in range(self.num_rows):
+    #             self.draw_square(start_x, start_y, size_of_square, self.grid[int(f'{row}{col}')][2])
+    #             start_x += size_of_square
+        
+
+# wn.exitonclick()
